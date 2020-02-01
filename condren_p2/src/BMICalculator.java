eb7cbaace2f97d78fae34c70d4418ac7ccd61e95
KyleCondren <59985079+KyleCondren@users.noreply.github.com>
2020-02-01 14:06:56 -0500

    Add files via upload

--- condren_p2/src/BMICalculator.java
+++ condren_p2/src/BMICalculator.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class BMICalculator {
 	
+	private Scanner input;
 	private String measureSystem;
 	private float meters;
 	private int feet;
 	private int inches;
 	private int weight;
 	private float BMI;
+	private String BMIcategory;
 	
 	public static void main(String[] args) {
 	    BMICalculator app = new BMICalculator();
@@ -18,32 +20,69 @@ public class BMICalculator {
 	    app.displayBmi();
 	}
 
-	public void readUserData() {
-		boolean goodMeasuringSystem = true;
-		do
-		{
-			System.out.print("Will you be using the Metric or Imperial measuring system?\n");
-			Scanner input = new Scanner(System.in);
-			measureSystem = input.next();
-			if(measureSystem.equalsIgnoreCase("Metric")) {
-				System.out.print("\nWhat is your height in meters?\n");
-				meters = input.nextFloat();
-				System.out.print("\nWhat is your weight in Kilograms?\n");
-				weight = input.nextInt();
-				break;
-			}
-			else if(measureSystem.equalsIgnoreCase("Imperial")) {
-				System.out.print("\nWhat is your height in Feet and Inches?\n");
-				feet = input.nextInt();
-				inches = input.nextInt();
-				System.out.print("\nWhat is your weight in Pounds?\n");
-				weight = input.nextInt();
-				break;
-			}
-			else
-				goodMeasuringSystem = false;
-			input.close();
-		} while (goodMeasuringSystem);
+	private void readUserData() {
+		readUnitType();
+		readMeasurementData();
+	}
+	
+	private void readUnitType() {
+		System.out.print("Will you be using the Metric or Imperial measuring system?\n");
+		input = new Scanner(System.in);
+		measureSystem = input.next();
+	}
+	
+	public void readMeasurementData() {
+		if(measureSystem.equalsIgnoreCase("Metric")) {
+			readMetricData();
+		}
+		else if(measureSystem.equalsIgnoreCase("Imperial")) {
+			readImperialData();
+		}
+		else
+			readUnitType();
+		input.close();
+	}
+	
+	private void readMetricData() {
+		getWeight();
+		getHeight();
+	}
+	
+	public void getHeight(){
+		if(measureSystem.equalsIgnoreCase("Metric"))
+			System.out.print("\nWhat is your height in meters?\n");
+		else
+			System.out.print("\nWhat is your height in Feet and Inches?\n");
+		setHeight();
+	}
+	public void getWeight() {
+		if(measureSystem.equalsIgnoreCase("Metric"))
+			System.out.print("\nWhat is your weight in Kilograms?\n");
+		else
+			System.out.print("\nWhat is your weight in Pounds?\n");
+		setWeight();
+	}
+	private void setHeight() {
+		if(measureSystem.equalsIgnoreCase("Metric")) {
+			meters = input.nextFloat();
+			if(meters < 0)
+				System.exit(0);
+		}
+		else {
+			feet = input.nextInt();
+			inches = input.nextInt();
+			if (feet < 0 || inches < 0)
+				System.exit(0);
+		}
+	}
+	
+	private void setWeight() {
+		weight = input.nextInt();
+	}
+	
+	private void readImperialData() {
+		getWeight();
+		getHeight();
 	}
 	
 	public void calculateBmi() {
@@ -53,10 +92,22 @@ public class BMICalculator {
 		}
 		if(measureSystem.equalsIgnoreCase("Metric")) 
 			BMI = weight/(meters*meters);
+		calculateBmiCategory();
 	}
 
+	private void calculateBmiCategory() {
+		if(BMI <= 18.5)
+			BMIcategory = "Underweight";
+		else if(18.5 < BMI && BMI < 24.9)
+			BMIcategory = "Normal Weight";
+		else if(25 < BMI && BMI < 29.9)
+			BMIcategory = "Overweight";
+		else
+			BMIcategory = "Obesity";
+	}
 	public void displayBmi( ) { 
 		String formatBMI = String.format("%.1f", BMI);
-		System.out.print("Your calcualted BMI is: "+formatBMI);
+		System.out.print("\nYour calcualted BMI is: "+formatBMI);
+		System.out.print("\nYour BMI category is: "+BMIcategory);
 	}
 }
